8a694f50976d981567fb2545b7392a2819d4ce82
Matthew Elvidge <SpartanC001@users.noreply.github.com>
2017-11-03 05:50:49 +0000

    Add files via upload

---
+++ removedAppids.js
@@ -0,0 +1,24 @@
+var removedAppids = {};
+
+removedAppids["448130"] = { appid: "448130", count: 5 };
+removedAppids["586090"] = { appid: "586090", count: 7 };
+removedAppids["392870"] = { appid: "392870", count: 5 };
+removedAppids["388870"] = { appid: "388870", count: 5 };
+removedAppids["314700"] = { appid: "314700", count: 5 };
+removedAppids["392050"] = { appid: "392050", count: 5 };
+removedAppids["486340"] = { appid: "486340", count: 5 };
+removedAppids["457940"] = { appid: "457940", count: 5 };
+removedAppids["383030"] = { appid: "383030", count: 5 };
+removedAppids["407740"] = { appid: "407740", count: 5 };
+removedAppids["407750"] = { appid: "407750", count: 5 };
+removedAppids["457930"] = { appid: "457930", count: 5 };
+removedAppids["491250"] = { appid: "491250", count: 5 };
+removedAppids["473620"] = { appid: "473620", count: 5 };
+removedAppids["473650"] = { appid: "473650", count: 5 };
+removedAppids["473640"] = { appid: "473640", count: 5 };
+removedAppids["473580"] = { appid: "473580", count: 5 };
+// 501690
+// 533710
+// 562710
+
+module.exports = removedAppids;
