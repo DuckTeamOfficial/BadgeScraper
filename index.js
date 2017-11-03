eda3b53ef043ce55c6b1ce4cfbb7d47cc9c3b83c
Matthew Elvidge <SpartanC001@users.noreply.github.com>
2017-11-03 09:37:44 +0000

    Update index.js
    Updated console.log texts to be more readable.

--- index.js
+++ index.js
@@ -6,32 +6,34 @@ var reqLimit    = 30; // Max simultaneous requests. Required bandwidth in Mbit/s
 
 request("http://api.steampowered.com/ISteamApps/GetAppList/v0002/", { json: true }, (err, res, body) => {
     if (!err && res.statusCode == 200 && body) {
-        console.log("## found " + body.applist.apps.length + " appids to scan!");
+        console.log("## Found " + body.applist.apps.length + " appids to scan for cards!");
         async.eachLimit(body.applist.apps, reqLimit, (app,callback) => {
             request("http://steamcommunity.com/id/palmdesert/gamecards/" + app.appid + "/", (err, res, body) => {
                 if (!err && res.statusCode == 200 && body) {
                     var count = (body.match(/game_card_ctn/g) || []).length;
                     if(count) {
                         data[app.appid] = { appid: app.appid, count: count };
+                        console.log("## Found appid " + app.appid + " with " + count + " cards, current sets found: " Object.keys(data).length);
+                    } else {
+                        console.log("## Skipping appid " + app.appid + " because it has no cards.");
                     }
-                    console.log("## appid " + app.appid + " has " + (count?count:"no") + " cards.");
                 } else {
-                    console.log("## an error occured getting badge data for appid " + appid + ": " + (err?err:res.statusCode));
+                    console.log("## An error occured getting badge data for appid " + appid + ": " + (err?err:res.statusCode));
                     process.exit();
                 }
                 callback();
             });
         }, function(err) {
-            console.log("## scanning complete, found " + Object.keys(data).length + " appids with cards.")
-            console.log("## writing data to file...");
+            console.log("## Scanning complete, found a total of " + Object.keys(data).length + " sets.")
+            console.log("## Writing set data to file...");
             fs.writeFile('./set_data.json', JSON.stringify(data), (err) => {
                 if(err) {
-                    console.log("## failed to write JSON file: " + err);
+                    console.log("## Failed to write JSON file: " + err);
                 }
-                console.log("## all data written to file.");
+                console.log("## All data written to file, mission complete!");
             });
         });
     } else {
-        console.log("## an error occured getting appids: " + (err?err:res.statusCode));
+        console.log("## An error occured getting appids: " + (err?err:res.statusCode));
     }
 });
