6672d0c18359e109297a3911428f4e1f23866908
DuckTeamOfficial <matthew.r.elvidge@gmail.com>
2017-11-23 01:22:33 +0000

    Update removedAppids.js
    added more appids to removed list

--- removedAppids.js
+++ removedAppids.js
@@ -60,5 +60,7 @@ removedAppids["581350"] = { appid: "581350", count: 5 };
 removedAppids["581690"] = { appid: "581690", count: 5 };
 removedAppids["582480"] = { appid: "582480", count: 5 };
 removedAppids["585720"] = { appid: "585720", count: 5 };
+removedAppids["501880"] = { appid: "501880", count: 5 };
+removedAppids["501800"] = { appid: "501800", count: 5 };
 
 module.exports = removedAppids;
