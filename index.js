2dc7196ca0a98c129fada558cb6a63b62c3ffca7
DuckTeamOfficial <matthew.r.elvidge@gmail.com>
2018-04-11 00:27:49 +0000

    Changed Progress Messages & Fixed Small bugs.

--- index.js
+++ index.js
@@ -12,22 +12,26 @@ https.get("https://api.steampowered.com/ISteamApps/GetAppList/v0002/", (res) => 
     });
     res.on('end', () => {
         if(res.statusCode == 200) {
+            var complete = 0, time = 0;
             body = JSON.parse(body);
-            console.log("## Found " + body.applist.apps.length + " appids to scan for cards!");
+            process.stdout.write("## Found " + body.applist.apps.length + " appids to scan for cards!" + '\n');
+            var printProgress = setInterval(() => {
+                process.stdout.write("## Time Elapsed: " + (time+=1) + "s. AppIDs Scanned: " + complete + "/" + body.applist.apps.length + ". Sets found: " + Object.keys(data).length + "." + '\r');
+            }, 1000);
             async.eachLimit(body.applist.apps, reqLimit, (app,callback) => {
+                complete++;
                 var retry = true;
                 async.whilst(() => {
                     return retry;
                 }, (cb) => {
                     https.get("https://steamcommunity.com/id/palmdesert/gamecards/" + app.appid + "/", (res) => {
-                        console.log(app.appid + ": " + res.statusCode);
-                        var body = '';
-                        res.setEncoding('utf8');
-                        res.on('data', (chunk) => {
-                            body += chunk;
-                        });
-                        res.on('end', () => {
-                            if(res.statusCode == 200) {
+                        if(res.statusCode == 200) {
+                            var body = '';
+                            res.setEncoding('utf8');
+                            res.on('data', (chunk) => {
+                                body += chunk;
+                            });
+                            res.on('end', () => {
                                 var cardCount = (body.match(/game_card_ctn/g) || []).length;
                                 var steamError = (body.match(/An error was encountered while processing your request:/g) || []).length;
                                 var invalidGame = (body.match(/Invalid game/g) || []).length;
@@ -42,43 +46,41 @@ https.get("https://api.steampowered.com/ISteamApps/GetAppList/v0002/", (res) => 
                                     retry = !invalidGame;
                                     cb();
                                 }
-                            } else if (res.statusCode == 302) {
-                                retry = false;
-                                cb();
-                            } else {
-                                console.log("## Received Response Code: " + res.statusCode);
-                                cb((res.statusCode==403)?true:false);
-                            }
-                        });
+                            });
+                        } else if (res.statusCode == 302) {
+                            retry = false;
+                            cb();
+                        } else {
+                            cb((res.statusCode==403)?true:false);
+                        }
                     }).on('error', (err) => {
-                        console.log("## Request Failed! " + err);
                         cb();
                     });
                 }, (err, res) => {
-                    if(!err && res) {
-                        console.log("## Badge found with appid " + res.appid + " and " + res.count + " cards, current total: " + Object.keys(data).length);
-                    }
                     callback(err);
                 });
-            }, (quit) => {
-                if (quit) {
-                    console.log("## Aborting! Got error 403, we are rate limited! Reduce reqLimit value!");
+            }, (e403) => {
+                clearInterval(printProgress);
+                process.stdout.clearLine();
+                if (e403) {
+                    process.stdout.write("## Aborting! Got error 403, we are rate limited! Reduce reqLimit value!" + '\n');
                     process.exit();
                 }
-                console.log("## Scanning complete, found a total of " + Object.keys(data).length + " sets.")
-                console.log("## Writing set data to file...");
+                process.stdout.write("## Scanning complete, found a total of " + Object.keys(data).length + " sets." + '\n')
+                process.stdout.write("## Writing set data to file..." + '\n');
                 fs.writeFile('./set_data.json', JSON.stringify(data), (err) => {
                     if(err) {
-                        console.log("## Failed to write JSON file: " + err);
+                        process.stdout.write("## Failed to write JSON file: " + err + '\n');
                     } else {
-                        console.log("## All data written to file, mission complete!");
+                        process.stdout.write("## All data written to file, mission complete!" + '\n');
                     }
+                    process.exit();
                 });
             });
         } else {
-            console.log("## Failed to get appid list! Status Code: " + res.statusCode);
+            process.stdout.write("## Failed to get appid list! Status Code: " + res.statusCode + '\n');
         }
     });
 }).on('error', (err) => {
-   console.log("## Request Failed! " + err);
+   process.stdout.write("## Request Failed! " + err + '\n');
 });
